f820c399c4f14608ff51f227f302a4480bbcd9f7
Nikita Churikov <nikita@chur.ru>
2023-07-08 23:13:55 +0200

    fixed link to traslated message

--- src/dn_telegram/bot.py
+++ src/dn_telegram/bot.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         title_original = f"*{title_original}*\n\n"
         description_original = news_item["description"] + "\n\n"
         description = f"{translate(description_original)}\n\n\n"
-        link_to_translation = f"htts://translate.google.com/translate?js=n&sl=auto&tl=en&u={news_item['link']}"
+        link_to_translation = f"https://translate.google.com/translate?js=n&sl=auto&tl=en&u={news_item['link']}"
 
         message = title_en + description + title_original + description_original + f"{news_item['publish_date']}\n" + f"{news_item['link']}\n" + link_to_translation
         bot.send_message(CHAT_ID, message, parse_mode="Markdown")
